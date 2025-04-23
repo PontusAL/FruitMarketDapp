268de4e8fe360e047748f480c3f869fd2c721b90
PontusAL <ponat404@student.liu.se>
2025-04-22 21:43:52 -0400

    removed hello world and added add fruit method

--- contracts/FruitMarketplace.sol
+++ contracts/FruitMarketplace.sol
@@ -2,7 +2,31 @@
 pragma solidity ^0.8.20;
 
 contract FruitMarketplace {
-    function helloWorld() public pure returns (string memory) {
-        return "Hello, Fruit Market!";
+
+    struct Fruit {
+        string name;
+        uint256 price;
+        address seller;
+        bool available;
+    }
+    Fruit[] public fruits;
+
+    function addFruit(string calldata name, uint256 price) public {
+        // Calldata uses less gas than memory, but cannot be modified. 
+        require(bytes(name).length > 0, "Name required");
+        require(price > 0, "Price must be positive");
+
+        fruits.push(Fruit({
+            name: name,
+            price: price,
+            seller: msg.sender,
+            available: true
+            })
+        );
+    }
+
+    function getFruits() public view returns (Fruit[] memory){
+        // Should this be public ?
+        return fruits;
     }
 }
